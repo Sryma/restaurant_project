03eac435f2876eb5020a91075a11d39bb63b84ff
Sryma <rurahgst@gmail.com>
2017-07-11 01:06:10 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(".flex-container h2").hover(function () {
 	$(this).fadeOut(2000);
 	$(this).fadeIn(2000);
 });
-$(".image h1").animateText([
-            {
-                offset: 0,
-                duration: 1000,
-                animation: "explode"
-            }
-    ]);
+
+		$(function(){
+	$(".shop").typed({
+		strings: ["Come ", "with.", "US" , "Come with us"],
+		typeSpeed: 2,
+	});
+});

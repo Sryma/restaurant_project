4c6271521c5318688354c917d612c60e0ffdf75b
Sryma <rurahgst@gmail.com>
2017-10-21 11:41:38 -0500

    first

--- js/main.js
+++ js/main.js
@@ -11,18 +11,3 @@
   });
 
 
-$(".coffee").hover(function () {
-	$(this).fadeOut(2000);
-	$(this).fadeIn(2000);
-});
-$(".flex-container h2").hover(function () {
-	$(this).fadeOut(2000);
-	$(this).fadeIn(2000);
-});
-
-		$(function(){
-	$(".shop").typed({
-		strings: ["Come ", "with.", "US" , "Come with us"],
-		typeSpeed: 2,
-	});
-});
